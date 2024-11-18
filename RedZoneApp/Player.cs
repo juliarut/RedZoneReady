f3816696d0d061c1896c0028bf8e8987541b4ebf
juliarut <julia.rutfjall@gmail.com>
2024-11-18 15:51:05 +0100

    add all players in national team

--- RedZoneApp/Player.cs
+++ RedZoneApp/Player.cs
@@ -2,25 +2,22 @@ using System;
 
 public class Player
 {    
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public string CyclePhase { get; set; } = "";
     public DateTime CycleStartDate { get; set; }
-    public int CycleLengthInDays { get; set; }
-
-    //public bool Consent { get; set; }
+    public int CycleLength { get; set; }
 
     public string GetCurrentPhase()
     {
         int daysSinceStart = (DateTime.Now - CycleStartDate).Days % CycleLength;
 
-    if (daysSinceStart >= 0 && daysSinceStart <= 5)
-        return "Red Alert";
-    if (daysSinceStart >= 6 && daysSinceStart <= 14)
-        return "Build Phase";
-    if (daysSinceStart >= 15 && daysSinceStart <= 16)
-        return "Power Peak";
-    
-    return "Steady Phase";
+        if (daysSinceStart >= 0 && daysSinceStart <= 5)
+            return "Red Alert";
+        if (daysSinceStart >= 6 && daysSinceStart <= 14)
+            return "Build Phase";
+        if (daysSinceStart >= 15 && daysSinceStart <= 16)
+            return "Power Peak";
+        
+        return "Steady Phase";
     }
-
 }
--- RedZoneApp/Program.cs
+++ RedZoneApp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var players = new List<Player>
+        {
+            new Player
+            {
+                Name = "Kosovare Asllani",
+                CycleStartDate = new DateTime(2023, 11, 1),
+                CycleLength = 28
+            },
+            new Player
+            {
+                Name = "Caroline Seger",
+                CycleStartDate = new DateTime(2023, 10, 25),
+                CycleLength = 29
+            },
+            new Player
+            {
+                Name = "Stina Blackstenius",
+                CycleStartDate = new DateTime(2023, 11, 3),
+                CycleLength = 27
+            },
+            new Player
+            {
+                Name = "Fridolina Rolfö",
+                CycleStartDate = new DateTime(2023, 11, 6),
+                CycleLength = 30
+            },
+            new Player
+            {
+                Name = "Magdalena Eriksson",
+                CycleStartDate = new DateTime(2023, 10, 30),
+                CycleLength = 26
+            }
+        };
+
+        
+        Console.WriteLine("Player Statuses:");
+        foreach (var player in players)
+        {
+            Console.WriteLine($"{player.Name} is in phase: {player.GetCurrentPhase()}");
+        }
+    }
+}
+
